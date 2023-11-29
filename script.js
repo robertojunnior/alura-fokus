949d0ae9d01d09e92a231f259c9acb5facdc4cd7
roberjunnior <roberjunior.dev@gmail.com>
2023-11-29 02:23:57 -0300

    update

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const html = document.querySelector("html");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
 
 focoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "foco");
+  alterarContexto("foco");
 });
 
 curtoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-curto");
+  alterarContexto("descanso-curto");
 });
 
 longoBt.addEventListener("click", () => {
-  html.setAttribute("data-contexto", "descanso-longo");
+  alterarContexto("descanso-longo");
 });
+
+function alterarContexto(contexto) {
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+}
