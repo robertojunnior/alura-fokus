276ee42ddfa84f7e55d70cf178084e4eef4fdab8
roberjunnior <roberjunior.dev@gmail.com>
2023-11-29 02:44:35 -0300

    update - innerHTML - Alterando os títulos

--- script.js
+++ script.js
@@ -20,4 +20,23 @@ longoBt.addEventListener("click", () => {
 function alterarContexto(contexto) {
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `Otimize sua produtividade,<br>
+      <strong class="app__title-strong"> mergulhe no que importa.</strong>`;
+      break;
+
+    case "descanso-curto":
+      titulo.innerHTML = `Que tal dar uma respirada?<br>
+      <strong class="app__title-strong"> Faça uma pausa curta!</strong>`;
+      break;
+
+    case "descanso-longo":
+      titulo.innerHTML = `Hora de voltar à superfície.<br>
+      <strong class="app__title-strong"> Faça uma pausa longa!</strong>`;
+      break;
+
+    default:
+      break;
+  }
 }
