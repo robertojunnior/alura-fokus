f1ef5a12b296e1d6d9b205c997318a065704169c
roberjunnior <roberjunior.dev@gmail.com>
2023-11-29 01:21:24 -0300

    update - evento botões cores de fundo

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const html = document.querySelector("html");
 const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
 
 focoBt.addEventListener("Click", () => {
   html.setAttribute("data-contexto", "foco");
 });
+
+curtoBt.addEventListener("Click", () => {
+  html.setAttribute("data-contexto", "descanso-curto")
+});
+
+longoBt.addEventListener("Click", () => {
+  html.setAttribute("data-contexto", "descanso-longo")
+});
+
