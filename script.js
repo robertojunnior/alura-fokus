4b62fe6f044f65e1689b69f4852d9538b31eb92c
roberjunnior <roberjunior.dev@gmail.com>
2023-11-30 04:46:07 -0300

    update - Inserindo musica no input com - new Audio

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const musica = new Audio("/sons/luna-rise-part-one.mp3");
+musica.loop = true;
+
+musicaFocoInput.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 focoBt.addEventListener("click", () => {
   alterarConteudo("foco");
